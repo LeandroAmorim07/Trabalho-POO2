b175f976cb9e3d74cfa5098df665a7adf50036cb
LeandroAmorim <75571536+JediNegao@users.noreply.github.com>
2024-02-26 13:38:19 -0300

    Add files via upload

---
+++ TrabalhoPOO2 V2/src/main/java/com/mycompany/trabalhopoo2/TrabalhoPOO2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabalhopoo2;
+
+/**
+ *
+ * @author 2022122760117
+ */
+public class TrabalhoPOO2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
